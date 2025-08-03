dc32c58cf4a235c6c1fc4ca87b49ae78ca5502b5
dimassaputra1006 <mdimas.sapuutra1006@gmail.com>
2025-08-03 16:08:16 +0700

    belajar list

---
+++ lamdafunctions.py
@@ -0,0 +1,15 @@
+my_list = [1, 2]
+
+my_list.append(3)
+print(my_list)
+
+print(my_list[0])
+
+my_list[0] = 0
+print(my_list)
+
+my_list.insert(1, 1)
+print(my_list)
+
+my_list.pop()
+print(my_list)
